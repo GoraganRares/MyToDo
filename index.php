18c308b084e11e6399819b815c92baa6195be27b
GorganRares <gorgan_rares@yahoo.ro>
2021-11-04 19:00:19 +0200

    ok

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
                     <button onclick="myFunction()" class="dropbtn nav-link">Comenzi</button>
                     <div id="myDropdown" class="dropdown-content">
                         <a href="analiza-discord.html">In asteptare</a>
-                        <a href="incepatori.html">Comenzi noi</a>
+                        <a href="incepatori.html">Cgomenzi noi</a>
                         <a href="intermediar.html">Comenzi terminate</a>
                     </div>
                 </li>
