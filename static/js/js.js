2fc8e099b480ed224de1ee7ca20ac9f67676c275
GorganRares <gorgan_rares@yahoo.ro>
2021-11-20 00:17:52 +0200

    da

--- static/js/js.js
+++ static/js/js.js
@@ -33,28 +33,6 @@ $(document).ready(function(){
   $('.hamburger').click(function(){
       $('nav').toggleClass("background");
   });
-  
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
   });
 });
 
@@ -93,3 +71,61 @@ window.onclick = function(event) {
     }
   }
 }
+
+$(document).ready(function(){
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar-scroll").style.top = "0";
+      document.getElementById("navbar-scroll").style.background = "rgba(0, 0, 0, 0.9)";
+      document.getElementById("navbar-scroll").style.transform= "translateY(0)";
+      document.getElementById("navbar-scroll").style.transition= "all 0.3s ease-in-out";
+    }
+    else {
+      document.getElementById("navbar-scroll").style.top = "-200px";
+    }
+    prevScrollpos = currentScrollPos;
+    if (currentScrollPos === 0) {
+      document.getElementById("navbar-scroll").style.backgroundColor = 'transparent';
+    } 
+  }
+});
+
+$(document).ready(function(){
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
